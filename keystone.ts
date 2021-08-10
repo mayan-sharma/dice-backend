4026cf175e3354752502baf43d5abd8a9d4ae881
Mayan <delhi.mayansharma@gmail.com>
2021-08-10 18:39:53 +0530

    feat: added CartItem schema

--- keystone.ts
+++ keystone.ts
@@ -5,6 +5,7 @@ import { withItemData, statelessSessions } from '@keystone-next/keystone/session
 import { User } from './schemas/User';
 import { Product } from './schemas/Product';
 import { ProductImage } from './schemas/ProductImage';
+import { CartItem } from './schemas/CartItem';
 import { insertSeedData } from './seed-data';
 import { sendEmail } from './lib/mail';
 
@@ -51,7 +52,8 @@ export default withAuth(config({
     lists: createSchema({
         User,
         Product,
-        ProductImage
+        ProductImage,
+        CartItem
     }),
     ui: {
         isAccessAllowed: ({ session }) => !!session?.data,
